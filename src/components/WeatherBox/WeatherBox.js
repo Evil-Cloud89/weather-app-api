ccb7ad81d0203c323b81e93a30934a50e734313d
Mateusz Oko <mo89mo@gmail.com>
2023-05-05 13:21:23 +0200

    Etap I - handle city name, fetch weather info

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,29 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+
+  const handleCityChange = useCallback((city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bcb95b3ec0a00f2407a89c48f97f490f&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        };
+        console.log(weatherData);
+      });
+  });
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
