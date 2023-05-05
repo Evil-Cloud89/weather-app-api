38440d683cd1aca28fe385a841ecac170f56439b
Mateusz Oko <mo89mo@gmail.com>
2023-05-05 15:11:10 +0200

    Etap II - Transfer data to WeatherSummary

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,9 +1,10 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = props => {
+  const [weather, setWeather] = useState();
 
   const handleCityChange = useCallback((city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bcb95b3ec0a00f2407a89c48f97f490f&units=metric`;
@@ -17,14 +18,14 @@ const WeatherBox = props => {
           icon: data.weather[0].icon,
           description: data.weather[0].main,
         };
-        console.log(weatherData);
+        setWeather(weatherData);
       });
   });
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      <WeatherSummary {...weather} />
       <Loader />
     </section>
   )
