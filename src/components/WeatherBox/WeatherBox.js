656db571c0cdcaf84eb4a17574b7d0821c09c738
Mateusz Oko <mo89mo@gmail.com>
2023-05-05 23:10:30 +0200

    Etap IV - Add ErrorBox

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,26 +2,36 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = props => {
   const [weather, setWeather] = useState();
   const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleCityChange = useCallback((city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=bcb95b3ec0a00f2407a89c48f97f490f&units=metric`;
 
     setPending(true);
+    setError(false);
+
     fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
-        setWeather(weatherData);
-        setPending(false);
+      .then((response) => {
+        if (response.status === 200) {
+          setError(false);
+          return response.json().then((data) => {
+            const weatherData = {
+              city: data.name,
+              temp: data.main.temp,
+              icon: data.weather[0].icon,
+              description: data.weather[0].main,
+            };
+            setWeather(weatherData);
+            setPending(false);
+          });
+        } else {
+          setError(true);
+        }
       });
   });
 
@@ -29,7 +39,8 @@ const WeatherBox = props => {
     <section>
       <PickCity action={handleCityChange} />
       { (weather && !pending) && <WeatherSummary {...weather} /> }
-      { (pending && pending) && <Loader /> }
+      { (pending && !error) && <Loader /> }
+      { error && <ErrorBox>There is no such city!</ErrorBox> }
     </section>
   )
 };
